ac16e1233482527a55a3dcebbb7e4fc5295667b6
Rodrigo Baltazar <rodrigobaltazarinf@gmail.com>
2024-11-06 00:00:32 -0300

    falta ajustes no layout e toda logica do jogo

---
+++ javascript.js
@@ -0,0 +1,8 @@
+
+    function resetGame() {
+        alert("O jogo será reiniciado!"); // Substitua com a lógica de reset do jogo
+    }
+
+    function goBack() {
+        window.location.href = "www.rodrigobaltazar.com.br"; // Substitua com a lógica de navegação
+    }
